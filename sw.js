db4dcb1b4d82b453c8557a4589bde9eeee56771a
YURLAK <yohosun@outlook.com>
2022-07-26 11:46:53 +0800

    Add musics

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220726.110322';
+const cacheName = 'chirpy-20220726.114547';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
