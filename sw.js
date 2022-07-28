1cea5b6614a63c32e5b16d1e7e622f943ef66051
YURLAK <yohosun@outlook.com>
2022-07-28 16:50:55 +0800

    七月版本最后更新

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220728.135007';
+const cacheName = 'chirpy-20220728.164956';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
