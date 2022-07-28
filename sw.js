cef8924531154b7458960e9c54113a0757f4aeee
YURLAK <yohosun@outlook.com>
2022-07-28 13:50:56 +0800

    New post

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220727.102126';
+const cacheName = 'chirpy-20220728.135007';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
