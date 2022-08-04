2b4d31bf47819706c7ea3a54e16be3d6074b1f52
YURLAK <yohosun@outlook.com>
2022-08-04 11:17:09 +0800

    Update

--- YURLAK.github.io/sw.js
+++ YURLAK.github.io/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220802.123121';
+const cacheName = 'chirpy-20220804.111634';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
