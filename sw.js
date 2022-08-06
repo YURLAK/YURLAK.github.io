2b6f53100a9be96988bce3a325f57b17d7f8b0e6
YURLAK <yohosun@outlook.com>
2022-08-06 11:12:40 +0800

    添加页面点击爱心效果

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220805.123231';
+const cacheName = 'chirpy-20220806.111153';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
