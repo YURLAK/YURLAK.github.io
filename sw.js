5644ba0fad99de589e253fcad966673167766487
YURLAK <yohosun@outlook.com>
2022-08-26 19:11:31 +0800

    出文，添加音乐**成都**和**Shape of you**

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220819.142755';
+const cacheName = 'chirpy-20220826.190927';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
