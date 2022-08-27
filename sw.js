15d8262c0e81a52c939cd7b80a1c22c188f149b1
YURLAK <yohosun@outlook.com>
2022-08-27 18:25:58 +0800

    Try again

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220826.190927';
+const cacheName = 'chirpy-20220827.182506';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
