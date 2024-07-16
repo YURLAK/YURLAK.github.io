f2e0ca0f6dfad7838d3593eceb8ea4748c197d01
Miles Sun <77920708+YURLAK@users.noreply.github.com>
2024-07-16 19:47:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,7 +50,7 @@ document.addEventListener("DOMContentLoaded", function() {
             },
             move: {
                 enable: true,
-                speed: 6,
+                speed: 3,
                 direction: "none",
                 random: false,
                 straight: false,
